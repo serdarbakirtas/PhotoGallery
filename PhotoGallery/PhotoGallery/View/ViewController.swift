7cfe48ebadb5d015b40ddbdb2f80b5526ecc7a0e
Hasan Serdar Bakirtas <serdarbakirtas@gmail.com>
2018-12-15 15:50:06 +0300

    initial project launched

---
+++ PhotoGallery/PhotoGallery/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Hasan Serdar on 15.12.2018.
+//  Copyright © 2018 Homify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PhotoGallery/Pods/Target Support Files/Pods-PhotoGalleryTests/Pods-PhotoGalleryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoGalleryTests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoGalleryTests
+@end
